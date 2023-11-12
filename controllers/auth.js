1dbfb9722eb8fc64888b96c6bf7807dc1ca22363
juan <juanjesusperezcastillo@gmail.com>
2023-11-12 15:31:13 -0300

    Carpeta models y schema User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,25 @@
-const register = (req,res) =>{
-    console.log('se ejecuto correctamente')
-    res.status(200).send({msg: 'todo ok'})
-}
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("../utils/jwt");
+
+async function register(req, res) {
+    const { firstname, lastname, email, password, avatar } = req.body;
+
+    if (!email) return res.status(400).send({ msg: "El email es obligatorio" });
+    if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
+
+    const user = new User({
+        firstname: firstname,
+        lastname: lastname,
+        email: email.toLowerCase(),
+        password: password,
+        active: "false",
+        role: "user",
+        avatar: avatar
+    });
+};
 
 module.exports = {
     register,
-}
+    login,
+};
--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const AuthController = require('../controllers/auth')
 
+// Cramos nuestras rutas
 const api = express.Router();
 
 api.post('/auth/register', AuthController.register)
