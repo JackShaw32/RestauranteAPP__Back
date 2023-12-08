a9018dac33957ffbc66a9344d00b29ef1103d686
juan <juanjesusperezcastillo@gmail.com>
2023-12-08 19:14:52 -0300

    Agregando la logica  para traer y actualizar los usuarios de la app

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,52 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
-async function getMe(req,res){
-    const {user_id} = req.user
-    const user = await User.findById(user_id)
+async function getMe(req, res) {
+  const { user_id } = req.user;
+  const user = await User.findById(user_id);
 
-    if(!user){
-        return res.status(400).send({msg: 'No se ha encontrado al usuario'})
-    }else {
-        return res.status(200).send(user)
+  if (!user) {
+    return res.status(400).send({ msg: "No se ha encontrado al usuario" });
+  } else {
+    return res.status(200).send(user);
+  }
+}
+
+async function getUsers(req, res) {
+  try {
+    const Users = await User.find();
+    res.status(200).send(Users);
+  } catch (err) {
+    res.status(400).send({ msg: "Error al obtener los usuarios" });
+  }
+}
+
+// Nueva función para actualizar el estado del usuario
+async function updateUserStatus(req, res) {
+  const { user_id } = req.params;
+  const { newStatus } = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      user_id,
+      { active: newStatus },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(400).send({ msg: "No se ha encontrado al usuario" });
     }
+
+    res.status(200).send(user);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ msg: "Error al actualizar el estado del usuario", err });
+  }
 }
 
 module.exports = {
-    getMe
-}
+  getMe,
+  getUsers,
+  updateUserStatus,
+};
--- router/user.js
+++ router/user.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const UserController = require('../controllers/user')
-const md_auth = require('../middlewares/authenticated')
+const express = require("express");
+const UserController = require("../controllers/user");
+const md_auth = require("../middlewares/authenticated");
 
 // Cramos nuestras rutas
 const api = express.Router();
 
-api.get('/user/me', [md_auth.asureAuth] ,UserController.getMe)
+api.put("/users/:user_id/status", UserController.updateUserStatus);
+api.get("/user/me", [md_auth.asureAuth], UserController.getMe);
+api.get("/users", UserController.getUsers);
 
-module.exports = api
+module.exports = api;
